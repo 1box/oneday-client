809510e530a7614928f5c2e3cee1e72df41bdfff
kimimaro <kimirius@gmail.com>
2014-06-10 07:48:59 +0800

    add swift

---
+++ Swift/OneDay/OneDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneDay
+//
+//  Created by YuTianhang on 14/6/9.
+//  Copyright (c) 2014年 onebox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
